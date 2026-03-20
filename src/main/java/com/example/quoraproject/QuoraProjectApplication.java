d156775e333d7170bd2bdd310d98994960a5b0bc
Rishabh Bahuguna <rishabhbahuguna2026@gmail.com>
2026-03-21 01:54:48 +0530

    Started the Quora Project

---
+++ src/main/java/com/example/quoraproject/QuoraProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.quoraproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoraProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoraProjectApplication.class, args);
+    }
+
+}
